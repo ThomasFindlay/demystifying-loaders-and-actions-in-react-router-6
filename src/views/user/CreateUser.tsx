64aecadc024247b9bf42c62ac6ed534c285a77d4
Thomas Findlay <thomasfindlay94@gmail.com>
2023-07-03 13:42:27 +0100

    Renamed onSubmit arg

--- src/views/user/CreateUser.tsx
+++ src/views/user/CreateUser.tsx
@@ -4,14 +4,15 @@ import UserForm from "./components/UserForm";
 const CreateUser = () => {
   const submit = useSubmit();
 
-  const onSubmit: React.FormEventHandler<HTMLFormElement> = e => {
-    e.preventDefault();
-    const formData = new FormData(e.target as HTMLFormElement);
+  const onSubmit: React.FormEventHandler<HTMLFormElement> = submitEvent => {
+    submitEvent.preventDefault();
+    const formData = new FormData(submitEvent.target as HTMLFormElement);
     submit(formData, {
       method: "post",
       action: "/user/create",
     });
   };
+
   return (
     <div className="max-w-sm py-24 mx-auto">
       <UserForm user={null} onSubmit={onSubmit} />
